a540635062b7c06f81ed60ee982a2c995bf90704
Daphne <daphnef@outlook.com>
2014-06-07 15:07:34 -0400

    Maintain the task list in code.  Pass it to the page.

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+var myTasks = [
+  'Target',
+  'Trader Joes',
+  'Laundry',
+  'Hook the garage up to the Internet'
+];
+
 /* GET home page. */
 router.get('/', function(req, res) {
   console.log(req.query);
+  
+  if (req.query.newTask) {
+    myTasks.push(req.query.newTask);
+  }
+  
   res.render('index', {
     title: 'WriteItDown',
-    newTask: req.query.newTask
+    taskList: myTasks,
   });
 });
 
