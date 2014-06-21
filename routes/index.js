e9e95328db31beb980940ab11bb76284d8357f47
Daphne <daphnef@outlook.com>
2014-06-21 15:21:23 -0400

    query and putItem.  Will see if the girls can figure out why the last item we put doesn't show up immediately...

--- routes/index.js
+++ routes/index.js
@@ -5,38 +5,66 @@ var AWS = require('aws-sdk');
 AWS.config.loadFromPath('./creds.aws')
 var dynamoDB = new AWS.DynamoDB();
 
+var tableName = 'WriteItDown';
+var clownie = 'clownie';
+
 /* GET home page. */
 router.get('/', function(req, res) {
   console.log(req.query);
   
-  var myTasks = []
-  
-  /*
+  // DynamoDB PutItem
   if (req.query.newTask) {
-    myTasks.push(req.query.newTask);
-  }*/
+    var now = new Date();
+    var sevenDaysFromNow = new Date();
+    sevenDaysFromNow.setDate(now.getDate() + 7);
+    var putParams = {
+      TableName: tableName,
+      Item: {
+        OwnerId: {
+          S: clownie
+        },
+        TaskTitle: {
+          S: req.query.newTask
+        },
+        TimeCreated: {
+          N: now.getTime().toString()
+        },
+        TimeDue: {
+          N: sevenDaysFromNow.getTime().toString()
+        }
+      }
+    };
+    dynamoDB.putItem(putParams, function(err, data) {
+      if (err) {
+        console.error(err, err.stack);
+      } else {
+        console.log("PutItem " + data);
+      }
+    });
+  }
   
   // DynamoDB Query
-  var params = {
-    TableName: 'WriteItDown',
+  var queryParams = {
+    TableName: tableName,
     Select: 'ALL_ATTRIBUTES',
     KeyConditions: {
       OwnerId: {
         ComparisonOperator: 'EQ',
         AttributeValueList: [
           {
-            S: 'clownie'
+            S: clownie
           }
         ]
       }
-    }
+    },
+    ConsistentRead: true
   };
-  dynamoDB.query(params, function(err, data) {
+  dynamoDB.query(queryParams, function(err, data) {
     if (err) {
       console.error(err, err.stack);
     } else {
-      console.log(data);
-      myTasks = data.Items.map(function(item) {
+      console.log("QueryItem " + data);
+      var myTasks = data.Items.map(function(item) {
         return item.TaskTitle.S;
       });
 
