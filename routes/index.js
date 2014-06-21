7e53dbfc90e09be6708463c0f3480b6a1462957f
Daphne <daphnef@outlook.com>
2014-06-21 14:54:00 -0400

    Query tasks from DynamoDB

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,54 @@
 var express = require('express');
 var router = express.Router();
 
-var myTasks = [
-  'Target',
-  'Trader Joes',
-  'Laundry',
-  'Hook the garage up to the Internet'
-];
+var AWS = require('aws-sdk');
+AWS.config.loadFromPath('./creds.aws')
+var dynamoDB = new AWS.DynamoDB();
 
 /* GET home page. */
 router.get('/', function(req, res) {
   console.log(req.query);
   
+  var myTasks = []
+  
+  /*
   if (req.query.newTask) {
     myTasks.push(req.query.newTask);
-  }
+  }*/
   
-  res.render('index', {
-    title: 'WriteItDown',
-    taskList: myTasks,
+  // DynamoDB Query
+  var params = {
+    TableName: 'WriteItDown',
+    Select: 'ALL_ATTRIBUTES',
+    KeyConditions: {
+      OwnerId: {
+        ComparisonOperator: 'EQ',
+        AttributeValueList: [
+          {
+            S: 'clownie'
+          }
+        ]
+      }
+    }
+  };
+  dynamoDB.query(params, function(err, data) {
+    if (err) {
+      console.error(err, err.stack);
+    } else {
+      console.log(data);
+      myTasks = data.Items.map(function(item) {
+        return item.TaskTitle.S;
+      });
+
+      console.log('There are ' + myTasks.length + ' tasks in myTasks');
+      
+      res.render('index', {
+        title: 'WriteItDown',
+        taskList: myTasks,
+      });
+    }
   });
+  
 });
 
 module.exports = router;
