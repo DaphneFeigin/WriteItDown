4cde1ee0c0f1223d8e4269b79ff69dffd0dd7b11
JumpingFuzzyUnicorns <lossy.moonbat@gmail.com>
2014-08-26 23:18:28 -0400

    button to create new task

--- public/javascripts/monkeys.js
+++ public/javascripts/monkeys.js
@@ -1,4 +1,37 @@
 $(function() {
     $("#tasks-accordion").accordion({collapsible: true});
+    
+    var createNewTaskDialog = $('#create-new-task-dialog').dialog({
+        autoOpen: false,
+        modal: true,
+        buttons: {
+            "Create": function() {
+                if (createNewTaskForm.checkValidity()) {
+                    createNewTaskForm.submit();
+                }
+            },
+            Cancel: function() {
+                createNewTaskForm.reset();
+                createNewTaskDialog.dialog('close');
+            }
+        }
+    });
+    
+    var createNewTaskForm = createNewTaskDialog.find('form')[0];
+    
+    $("button#create-new-task")
+        .button({
+            icons: {
+                primary: 'ui-icon-plus'
+            }
+        })
+        .click(function(event) {
+            createNewTaskDialog.dialog('open');
+        });
+        
+    $("#due-date-picker").datepicker({
+        constrainInput: true,
+        minDate: new Date()
+    });
 });
 
