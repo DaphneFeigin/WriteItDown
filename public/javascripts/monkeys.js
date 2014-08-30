f3df6e6f81357f48f19581501fd862039ec59eda
JumpingFuzzyUnicorns <lossy.moonbat@gmail.com>
2014-08-30 14:10:03 -0400

    checkbox

--- public/javascripts/monkeys.js
+++ public/javascripts/monkeys.js
@@ -34,16 +34,21 @@ $(function() {
     
     var createNewTaskForm = createNewTaskDialog.find('form')[0];
     
-    $("button#create-new-task")
-        .button({
-            icons: {
-                primary: 'ui-icon-plus'
-            }
-        })
-        .click(function(event) {
-            createNewTaskDialog.dialog('open');
-        });
-        
+    $("button#create-new-task").button({
+        icons: {
+            primary: 'ui-icon-plus'
+        }
+    }).click(function(event) {
+        createNewTaskDialog.dialog('open');
+    });
+    
+    $("button.task-check-complete").button({
+        icons: {
+            primary: 'ui-icon-check'
+        },
+        text: false
+    });
+
     $("#new-task-due-date-picker").datepicker({
         constrainInput: true,
         minDate: new Date()
