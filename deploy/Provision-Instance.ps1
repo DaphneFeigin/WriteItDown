e6ed9db3f7d753d9f2827615f739cd2b859f3274
Piso Mojado Grande <becky_weiss@hotmail.com>
2014-09-06 23:07:29 -0400

    IAM instance profile.  Credentials on instance not recognized

--- deploy/Provision-Instance.ps1
+++ deploy/Provision-Instance.ps1
@@ -2,6 +2,10 @@ $ami = "ami-76817c1e"
 $subnetId = "subnet-f88472a1"
 $sgId = "sg-fa82dc9f"
 $keyName = "MIKE_inst"
-$cloudInitFile = "file://c:/Users/Becky/Documents/GitHub/WriteItDown/deploy/cloud.init"
+$instanceProfileArn = "arn:aws:iam::516893983603:instance-profile/WriteItDownService" 
+$scriptPath = Split-Path $MyInvocation.MyCommand.Path
+$cloudInitFile = Join-Path $scriptPath "cloud.init"
+Write-Output "Launching instance with init script $cloudInitFile"
+Get-Content $cloudInitFile
 
-aws ec2 run-instances --image-id $ami --security-group-ids $sgId --user-data $cloudInitFile --instance-type t2.micro --subnet-id $subnetId --key-name $keyName
+aws ec2 run-instances --image-id $ami --iam-instance-profile Arn=$instanceProfileArn --security-group-ids $sgId --user-data $cloudInitFile --instance-type t2.micro --subnet-id $subnetId --key-name $keyName
