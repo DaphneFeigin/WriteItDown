5a5c00d9fb74cc0f5db832bcf3505eb8f695664c
JumpingFuzzyUnicorns <lossy.moonbat@gmail.com>
2014-09-13 17:01:27 -0400

    split Publish-Service

--- deploy/Publish-Service.ps1
+++ deploy/Publish-Service.ps1
@@ -1,22 +1,11 @@
-$sourceDir = "C:\Users\Becky\Documents\GitHub\WriteItDown"
-$tmpZip = "C:\temp\tmp.zip"
+$zipFile = $args[0]
 $s3File = "s3://WriteItDown/latest.zip"
-
-Add-Type -AssemblyName System.IO.Compression
-Add-Type -AssemblyName System.IO.Compression.FileSystem
-$compressionLevel = [System.IO.Compression.CompressionLevel]::Optimal
-Write-Output "Zipping $sourceDir to $tmpZip..."
-if (Test-Path $tmpZip) {
-    Remove-Item $tmpZip
-}
-[System.IO.Compression.ZipFile]::CreateFromDirectory($sourceDir, $tmpZip, $compressionLevel, $false)
-Write-Output "Done zipping $tmpZip"
 
 $s3FileOld = $s3File + "." + (Get-Date -UFormat "%Y-%m-%d-%H-%M");
 Write-Output "Copying $s3File to $s3FileOld..."
 & aws s3 cp $s3File $s3FileOld 
-Write-Output "Copying $tmpZip to $s3File..."
-& aws s3 cp $tmpZip $s3File
+Write-Output "Copying $zipFile to $s3File..."
+& aws s3 cp $zipFile $s3File
 Write-Output "Done copying to $s3File"
 
 
---
+++ deploy/ZipAndPublish-Service.ps1
@@ -0,0 +1,15 @@
+$sourceDir = "C:\Users\Becky\Documents\GitHub\WriteItDown"
+$tmpZip = "C:\temp\tmp.zip"
+
+Add-Type -AssemblyName System.IO.Compression
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+$compressionLevel = [System.IO.Compression.CompressionLevel]::Optimal
+Write-Output "Zipping $sourceDir to $tmpZip..."
+if (Test-Path $tmpZip) {
+    Remove-Item $tmpZip
+}
+[System.IO.Compression.ZipFile]::CreateFromDirectory($sourceDir, $tmpZip, $compressionLevel, $false)
+Write-Output "Done zipping $tmpZip"
+
+& Publish-Service.ps1 $tmpZip
+
