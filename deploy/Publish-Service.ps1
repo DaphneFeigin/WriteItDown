d4d03caf99747cf78edcd65f76540354f48180b5
Piso Mojado Grande <becky_weiss@hotmail.com>
2014-09-13 13:25:46 -0400

    keep old versions

--- deploy/Publish-Service.ps1
+++ deploy/Publish-Service.ps1
@@ -1,6 +1,6 @@
 $sourceDir = "C:\Users\Becky\Documents\GitHub\WriteItDown"
 $tmpZip = "C:\temp\tmp.zip"
-$s3Bucket = "s3://WriteItDown/latest.zip"
+$s3File = "s3://WriteItDown/latest.zip"
 
 Add-Type -AssemblyName System.IO.Compression
 Add-Type -AssemblyName System.IO.Compression.FileSystem
@@ -12,8 +12,11 @@ if (Test-Path $tmpZip) {
 [System.IO.Compression.ZipFile]::CreateFromDirectory($sourceDir, $tmpZip, $compressionLevel, $false)
 Write-Output "Done zipping $tmpZip"
 
-Write-Output "Copying $tmpZip to $s3Bucket..."
-& aws s3 cp $tmpZip $s3Bucket
-Write-Output "Done copying to $s3Bucket"
+$s3FileOld = $s3File + "." + (Get-Date -UFormat "%Y-%m-%d-%H-%M");
+Write-Output "Copying $s3File to $s3FileOld..."
+& aws s3 cp $s3File $s3FileOld 
+Write-Output "Copying $tmpZip to $s3File..."
+& aws s3 cp $tmpZip $s3File
+Write-Output "Done copying to $s3File"
 
 
