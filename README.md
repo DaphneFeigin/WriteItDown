750362f60f44d42238513f7e2624393d2ef1b182
JumpingFuzzyUnicorns <lossy.moonbat@gmail.com>
2014-09-13 16:36:17 -0400

    Use EC2 role credentials by default

--- README.md
+++ README.md
@@ -4,9 +4,19 @@ WriteItDown
 To run on a port other than the default 80, e.g. 3000:
  npm config set TaskWebsite:port 3000
 
+AWS credentials are obtained from the EC2 role by default.  To supply those credentials from a file on your machine:
+ npm config set TaskWebsite:awsCreds c:\temp\MyCreds.aws
+MyCreds.aws is a file with the following JSON:
+ {
+   "accessKey":"YOUR_ACCESS_KEY_ID",
+   "secretAccessKey":"YOUR_SECRET_KEY",
+   "region":"us-east-1"
+ }
+
+
 Logs go to /var/log/WriteItDown.log by default.
 To change it to log to stderr:
  npm config set TaskWebsite:logging CONSOLE
 To change it to log to a particular file
  npm config set TaskWebsite:logging FILE
- npm config set TaskWebsite:logFile "c:\\temp\\MyLog.log"
+ npm config set TaskWebsite:logFile c:\temp\MyLog.log
--- models/dynamiteTNT.js
+++ models/dynamiteTNT.js
@@ -4,13 +4,18 @@ var AWS = require('aws-sdk');
 
 var log = require('npmlog');
 
-// creds.aws is expected in the root directory and looks like this:
-//{
-//  "accessKeyId":"YOUR_ACCESS_KEY_ID",
-//  "secretAccessKey":"YOUR_SECRET_KEY",
-//  "region":"us-east-1"
-//}
-AWS.config.loadFromPath('./creds.aws');
+// To supply credentials from a file, specify it with "npm config set TaskWebsite:awsCreds path_to_file"
+// This file should be in the format:
+// {
+//   "accessKey":"YOUR_ACCESS_KEY_ID",
+//   "secretAccessKey":"YOUR_SECRET_KEY",
+//   "region":"us-east-1"
+// }
+// Otherwise, the EC2 role credentials will be used
+if (process.env.npm_package_config_awsCreds) {
+    log.info("Using credentials at " + process.env.npm_package_config_awsCreds);
+    AWS.config.loadFromPath(process.env.npm_package_config_awsCreds);
+}
 
 var dynamoDB = new AWS.DynamoDB();
 
