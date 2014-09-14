81f34e4dbc414a6eb4a49a5c8619d42388d0e55f
Piso Mojado Grande <becky_weiss@hotmail.com>
2014-09-13 22:54:49 -0400

    Get public IP of running instance

--- deploy/Provision-Instance.ps1
+++ deploy/Provision-Instance.ps1
@@ -8,4 +8,21 @@ $cloudInitFile = Join-Path $scriptPath "cloud.init"
 Write-Output "Launching instance with init script $cloudInitFile"
 Get-Content $cloudInitFile
 
-aws ec2 run-instances --image-id $ami --iam-instance-profile Arn=$instanceProfileArn --security-group-ids $sgId --user-data file://$cloudInitFile --instance-type t2.micro --subnet-id $subnetId --key-name $keyName
+$runInstanceResponse = [string](aws ec2 run-instances --image-id $ami --iam-instance-profile Arn=$instanceProfileArn --security-group-ids $sgId --user-data file://$cloudInitFile --instance-type t2.micro --subnet-id $subnetId --key-name $keyName --output json)
+
+$instanceObj = ConvertFrom-Json $runInstanceResponse;
+$instanceId = $instanceObj.Instances[0].InstanceId;
+Write-Output "Your instance: $instanceId"
+
+Write-Output "Wait for running..."
+do {
+    $instanceDescResponse = [string](aws ec2 describe-instances --instance-ids $instanceId --output json)
+    $instanceDesc = ConvertFrom-Json $instanceDescResponse
+    $state = $instanceDesc.Reservations[0].Instances[0].State.Name;
+    if ($state.CompareTo("running")) {
+        Sleep 5
+    }
+} until (-not ($state.CompareTo("running")))
+
+$publicIp = $instanceDesc.Reservations[0].Instances[0].PublicIpAddress
+Write-Output "Running.  Public IP $publicIp"
