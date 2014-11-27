8c895bd23c2039dd3521213017c94cd803ab92f9
Piso Mojado Grande <becky_weiss@hotmail.com>
2014-11-27 13:10:34 -0500

    Move button and h2 to tasklist

--- public/javascripts/monkeys.js
+++ public/javascripts/monkeys.js
@@ -13,51 +13,6 @@ $(function() {
         }
     });
     
-    var createNewTaskDialog = $('#create-new-task-dialog').dialog({
-        autoOpen: false,
-        modal: true,
-        buttons: {
-            "Create": function() {
-                if (createNewTaskForm.checkValidity()) {
-                    ajaxWithAuthCheck({
-                        url: "/tasks/new",
-                        data: {
-                            name: $('#new-task-title').val(),
-                            dateDue: $('#new-task-due-date-picker').val(),
-                            notes: $('#new-task-notes').val()
-                        },
-                        dataType: "json",
-                        type: "POST",
-                        success: function(data, textStatus, jqxhr) {
-                            location = location;
-                        },
-                        error: function(jqxhr, textStatus, errorThrown) {
-                            alert("error " + textStatus + " : " + errorThrown);
-                        }
-                    });
-                }
-            },
-            Cancel: function() {
-                createNewTaskForm.reset();
-                createNewTaskDialog.dialog('close');
-            }
-        },
-    });
-    
-    var createNewTaskForm = createNewTaskDialog.find('form')[0];
-    
-    $("button#create-new-task").button({
-        icons: {
-            primary: 'ui-icon-plus'
-        }
-    }).click(function(event) {
-        createNewTaskDialog.dialog('open');
-    });
-    
-    $("#new-task-due-date-picker").datepicker({
-        constrainInput: true,
-        minDate: new Date()
-    });
     
     $("#sign-out").click(function() {
         signOut(); 
